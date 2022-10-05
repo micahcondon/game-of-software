426f5fbbe7b001af51f21823353282c37cb42261
Micah Condon <mcondon@trainingpeaks.com>
2022-10-04 19:13:13 -0600

    added isValidMove

--- tic-tac-toe/basic-algorithms-and-data-structures/algorithms.js
+++ tic-tac-toe/basic-algorithms-and-data-structures/algorithms.js
@@ -4,3 +4,10 @@ export const O = 2
 export function whoseTurnIsIt(moves) {
   return moves.size % 2 === 0 ? X : O
 }
+
+export function isValidMove(move, boardSize, moves) {
+  return Number.isInteger(move)
+    && move >= 1
+    && move <= boardSize * boardSize
+    && !moves.has(move)
+}
--- tic-tac-toe/basic-algorithms-and-data-structures/test.js
+++ tic-tac-toe/basic-algorithms-and-data-structures/test.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import { describe, it } from 'node:test'
-import { X, O, whoseTurnIsIt } from './algorithms.js'
+import { X, O, whoseTurnIsIt, isValidMove } from './algorithms.js'
 
 describe('whoseTurnIsIt', () => {
 
@@ -19,4 +19,32 @@ describe('whoseTurnIsIt', () => {
     assert.equal(who, O)
   })
 
+})
+
+describe('isValidMove', () => {
+
+  it('Returns false if move is not an integer', () => {
+    const valid = isValidMove(0.5, 3, new Set())
+    assert.equal(valid, false)
+  })
+
+  it('Returns false if move is < 1', () => {
+    const valid = isValidMove(0, 3, new Set())
+    assert.equal(valid, false)
+  })
+
+  it('Returns false if move is greater than board size', () => {
+    const valid = isValidMove(10, 3, new Set())
+    assert.equal(valid, false)
+  })
+
+  it('Returns false if move is already taken', () => {
+    const valid = isValidMove(7, 3, new Set([1,7,3,9]))
+    assert.equal(valid, false)
+  })
+
+  it('Returns true if move is within board boundaries and not already taken', () => {
+    const valid = isValidMove(5, 3, new Set([1,7,3,9]))
+    assert.equal(valid, true)
+  })
 })
