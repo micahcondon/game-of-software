9a49cdffe2c93d0be47d6fc584ab2a179187e71f
Micah Condon <mcondon@trainingpeaks.com>
2022-10-04 19:01:20 -0600

    basic test structure

--- tic-tac-toe/basic-algorithms-and-data-structures/test.js
+++ tic-tac-toe/basic-algorithms-and-data-structures/test.js
@@ -1,8 +1,21 @@
 import assert from 'assert'
 import { describe, it } from 'node:test'
+import { X, O, whoseTurnIsIt } from './algorithms.js'
 
-describe('my thing', () => {
-  it('should work', () => {
-    assert.ok(true)
+describe('whoseTurnIsIt', () => {
+
+  it('Returns X when moves list is empty', () => {
+    const who = whoseTurnIsIt([])
+    assert.equal(who, X)
+  })
+
+  it('Returns X when moves list length is even', () => {
+    const who = whoseTurnIsIt([2,3,5,7])
+    assert.equal(who, X)
+  })
+
+  it('Returns O when moves list length is odd', () => {
+    const who = whoseTurnIsIt([2,3,5])
+    assert.equal(who, O)
   })
 })
